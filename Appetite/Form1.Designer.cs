8c167f8c3954e20f3635e1e6fba5835e1aae076d
aniruddha-ramgir <aniruddha.ramgir@gmail.com>
2016-11-06 13:36:53 +0530

    Version 1

---
+++ Appetite/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace Appetite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genderLabel = new System.Windows.Forms.TextBox();
+            this.WeightLabel = new System.Windows.Forms.TextBox();
+            this.heightLabel = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.mainButton = new System.Windows.Forms.Button();
+            this.age = new System.Windows.Forms.TextBox();
+            this.cWeight = new System.Windows.Forms.TextBox();
+            this.inch = new System.Windows.Forms.TextBox();
+            this.feet = new System.Windows.Forms.TextBox();
+            this.iWeight = new System.Windows.Forms.TextBox();
+            this.cIntake2 = new System.Windows.Forms.TextBox();
+            this.cIntake1 = new System.Windows.Forms.TextBox();
+            this.fIntake2 = new System.Windows.Forms.TextBox();
+            this.pIntake2 = new System.Windows.Forms.TextBox();
+            this.fIntake1 = new System.Windows.Forms.TextBox();
+            this.pIntake1 = new System.Windows.Forms.TextBox();
+            this.gender = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.genderLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.genderLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderLabel.Location = new System.Drawing.Point(19, 83);
+            this.genderLabel.Margin = new System.Windows.Forms.Padding(10);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.ReadOnly = true;
+            this.genderLabel.Size = new System.Drawing.Size(100, 22);
+            this.genderLabel.TabIndex = 0;
+            this.genderLabel.Text = "Gender ";
+            // 
+            // WeightLabel
+            // 
+            this.WeightLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.WeightLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.WeightLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WeightLabel.Location = new System.Drawing.Point(19, 125);
+            this.WeightLabel.Margin = new System.Windows.Forms.Padding(10);
+            this.WeightLabel.Name = "WeightLabel";
+            this.WeightLabel.ReadOnly = true;
+            this.WeightLabel.Size = new System.Drawing.Size(100, 22);
+            this.WeightLabel.TabIndex = 1;
+            this.WeightLabel.Text = "Weight ";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.heightLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.heightLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightLabel.Location = new System.Drawing.Point(19, 167);
+            this.heightLabel.Margin = new System.Windows.Forms.Padding(10);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.ReadOnly = true;
+            this.heightLabel.Size = new System.Drawing.Size(100, 22);
+            this.heightLabel.TabIndex = 2;
+            this.heightLabel.Text = "Height [ft-in] ";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.ageLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ageLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageLabel.Location = new System.Drawing.Point(19, 41);
+            this.ageLabel.Margin = new System.Windows.Forms.Padding(10);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.ReadOnly = true;
+            this.ageLabel.Size = new System.Drawing.Size(100, 22);
+            this.ageLabel.TabIndex = 3;
+            this.ageLabel.Text = "Age ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(19, 335);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "Fat Intake ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(19, 293);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.Text = "Protein Intake ";
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(19, 251);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.Text = "Calorie Intake ";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(19, 209);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.Text = "Ideal Weight ";
+            // 
+            // mainButton
+            // 
+            this.mainButton.Location = new System.Drawing.Point(52, 375);
+            this.mainButton.Name = "mainButton";
+            this.mainButton.Size = new System.Drawing.Size(201, 32);
+            this.mainButton.TabIndex = 9;
+            this.mainButton.Text = "CALCULATE";
+            this.mainButton.UseVisualStyleBackColor = true;
+            this.mainButton.Click += new System.EventHandler(this.mainButton_Click_1);
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(237, 41);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(64, 20);
+            this.age.TabIndex = 10;
+            // 
+            // cWeight
+            // 
+            this.cWeight.Location = new System.Drawing.Point(231, 128);
+            this.cWeight.Name = "cWeight";
+            this.cWeight.Size = new System.Drawing.Size(70, 20);
+            this.cWeight.TabIndex = 11;
+            // 
+            // inch
+            // 
+            this.inch.Location = new System.Drawing.Point(231, 170);
+            this.inch.Name = "inch";
+            this.inch.Size = new System.Drawing.Size(70, 20);
+            this.inch.TabIndex = 14;
+            // 
+            // feet
+            // 
+            this.feet.Location = new System.Drawing.Point(147, 170);
+            this.feet.Name = "feet";
+            this.feet.Size = new System.Drawing.Size(70, 20);
+            this.feet.TabIndex = 15;
+            // 
+            // iWeight
+            // 
+            this.iWeight.BackColor = System.Drawing.SystemColors.Control;
+            this.iWeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.iWeight.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iWeight.Location = new System.Drawing.Point(231, 209);
+            this.iWeight.Margin = new System.Windows.Forms.Padding(10);
+            this.iWeight.Name = "iWeight";
+            this.iWeight.ReadOnly = true;
+            this.iWeight.Size = new System.Drawing.Size(70, 29);
+            this.iWeight.TabIndex = 16;
+            // 
+            // cIntake2
+            // 
+            this.cIntake2.BackColor = System.Drawing.SystemColors.Control;
+            this.cIntake2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cIntake2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cIntake2.Location = new System.Drawing.Point(231, 251);
+            this.cIntake2.Margin = new System.Windows.Forms.Padding(10);
+            this.cIntake2.Name = "cIntake2";
+            this.cIntake2.ReadOnly = true;
+            this.cIntake2.Size = new System.Drawing.Size(70, 29);
+            this.cIntake2.TabIndex = 17;
+            // 
+            // cIntake1
+            // 
+            this.cIntake1.BackColor = System.Drawing.SystemColors.Control;
+            this.cIntake1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cIntake1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cIntake1.Location = new System.Drawing.Point(147, 251);
+            this.cIntake1.Margin = new System.Windows.Forms.Padding(10);
+            this.cIntake1.Name = "cIntake1";
+            this.cIntake1.ReadOnly = true;
+            this.cIntake1.Size = new System.Drawing.Size(70, 29);
+            this.cIntake1.TabIndex = 18;
+            // 
+            // fIntake2
+            // 
+            this.fIntake2.BackColor = System.Drawing.SystemColors.Control;
+            this.fIntake2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fIntake2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fIntake2.Location = new System.Drawing.Point(231, 333);
+            this.fIntake2.Margin = new System.Windows.Forms.Padding(10);
+            this.fIntake2.Name = "fIntake2";
+            this.fIntake2.ReadOnly = true;
+            this.fIntake2.Size = new System.Drawing.Size(70, 29);
+            this.fIntake2.TabIndex = 19;
+            // 
+            // pIntake2
+            // 
+            this.pIntake2.BackColor = System.Drawing.SystemColors.Control;
+            this.pIntake2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pIntake2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pIntake2.Location = new System.Drawing.Point(231, 293);
+            this.pIntake2.Margin = new System.Windows.Forms.Padding(10);
+            this.pIntake2.Name = "pIntake2";
+            this.pIntake2.ReadOnly = true;
+            this.pIntake2.Size = new System.Drawing.Size(70, 29);
+            this.pIntake2.TabIndex = 20;
+            // 
+            // fIntake1
+            // 
+            this.fIntake1.BackColor = System.Drawing.SystemColors.Control;
+            this.fIntake1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fIntake1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fIntake1.Location = new System.Drawing.Point(147, 333);
+            this.fIntake1.Margin = new System.Windows.Forms.Padding(10);
+            this.fIntake1.Name = "fIntake1";
+            this.fIntake1.ReadOnly = true;
+            this.fIntake1.Size = new System.Drawing.Size(70, 29);
+            this.fIntake1.TabIndex = 21;
+            // 
+            // pIntake1
+            // 
+            this.pIntake1.BackColor = System.Drawing.SystemColors.Control;
+            this.pIntake1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pIntake1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pIntake1.Location = new System.Drawing.Point(147, 293);
+            this.pIntake1.Margin = new System.Windows.Forms.Padding(10);
+            this.pIntake1.Name = "pIntake1";
+            this.pIntake1.ReadOnly = true;
+            this.pIntake1.Size = new System.Drawing.Size(70, 29);
+            this.pIntake1.TabIndex = 22;
+            // 
+            // gender
+            // 
+            this.gender.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gender.FormattingEnabled = true;
+            this.gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.gender.Location = new System.Drawing.Point(177, 83);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(124, 25);
+            this.gender.TabIndex = 23;
+            this.gender.SelectedIndexChanged += new System.EventHandler(this.gender_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.mainButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 412);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.pIntake1);
+            this.Controls.Add(this.fIntake1);
+            this.Controls.Add(this.pIntake2);
+            this.Controls.Add(this.fIntake2);
+            this.Controls.Add(this.cIntake1);
+            this.Controls.Add(this.cIntake2);
+            this.Controls.Add(this.iWeight);
+            this.Controls.Add(this.feet);
+            this.Controls.Add(this.inch);
+            this.Controls.Add(this.cWeight);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.mainButton);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.WeightLabel);
+            this.Controls.Add(this.genderLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox genderLabel;
+        private System.Windows.Forms.TextBox WeightLabel;
+        private System.Windows.Forms.TextBox heightLabel;
+        private System.Windows.Forms.TextBox ageLabel;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.TextBox cWeight;
+        private System.Windows.Forms.TextBox inch;
+        private System.Windows.Forms.TextBox feet;
+        private System.Windows.Forms.TextBox iWeight;
+        private System.Windows.Forms.TextBox cIntake2;
+        private System.Windows.Forms.TextBox cIntake1;
+        private System.Windows.Forms.TextBox fIntake2;
+        private System.Windows.Forms.TextBox pIntake2;
+        private System.Windows.Forms.TextBox fIntake1;
+        private System.Windows.Forms.TextBox pIntake1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.ComboBox gender;
+    }
+}
+
---
+++ Appetite/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appetite
+{
+    public partial class Form1 : Form
+    {
+        int ob;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mainButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void gender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ob = gender.SelectedIndex;
+        }
+
+        private void mainButton_Click_1(object sender, EventArgs e)
+        {
+            int feet_here;
+            int.TryParse(feet.Text, out feet_here);
+            if ((age.Text != "") && (cWeight.Text != "") && (inch.Text != "") && ((feet_here > 5) || (feet_here == 5)))
+            {
+                double ibw_value = 0, Weight_lbs = 0, Calo_value1 = 0, Calo_value2 = 0;
+                int age_here = int.Parse(age.Text);
+                int weight_here = int.Parse(cWeight.Text);
+                int inch_here = int.Parse(inch.Text);
+                if (ob == 0)
+                { //Check gender and find IBW
+                    ibw_value = (50 + (2.3 * inch_here));
+                    Weight_lbs = (ibw_value * 2.20462);
+                    Calo_value1 = Weight_lbs * 13; //set Calories1	
+                    Calo_value2 = Weight_lbs * 15; //set Calories2	
+                }
+                else
+                {
+                    ibw_value = ((45.5) + (2.3 * inch_here));
+                    Weight_lbs = (ibw_value * 2.20462);
+                    Calo_value1 = Weight_lbs * 10; //set Calories1
+                    Calo_value2 = Weight_lbs * 14; //set Calories2
+                }
+                iWeight.Text = Convert.ToString(ibw_value);
+                cIntake1.Text = Convert.ToString(Calo_value1);
+                cIntake2.Text = Convert.ToString(Calo_value2);
+                pIntake1.Text = Convert.ToString(ibw_value * 0.8); 
+                pIntake2.Text = Convert.ToString(ibw_value * 1.8); 
+                fIntake1.Text = Convert.ToString((Calo_value1 * 0.30) / 9);
+                fIntake2.Text = Convert.ToString((Calo_value2 * 0.30) / 9);
+            }
+            else
+            {
+                MessageBox.Show("Check Inputs");
+            }
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
